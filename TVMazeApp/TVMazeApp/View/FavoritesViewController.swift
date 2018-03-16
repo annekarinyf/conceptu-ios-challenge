a6cf09db2ac7448a15b30043f08ff9547f64c4d8
Anne Kariny Silva Freitas <kariny.freitas@acad.ifce.edu.br>
2018-03-15 23:14:22 -0300

    Create a ViewControllers for future features

---
+++ TVMazeApp/TVMazeApp/View/FavoritesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesViewController.swift
+//  TVMazeApp
+//
+//  Created by Anne Kariny Silva Freitas on 15/03/2018.
+//  Copyright © 2018 Anne Kariny Silva Freitas. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
---
+++ TVMazeApp/TVMazeApp/View/TabBarController.swift
@@ -0,0 +1,13 @@
+//
+//  TabBarController.swift
+//  TVMazeApp
+//
+//  Created by Anne Kariny Silva Freitas on 15/03/2018.
+//  Copyright © 2018 Anne Kariny Silva Freitas. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+}
