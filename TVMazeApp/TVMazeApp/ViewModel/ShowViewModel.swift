b8b026f14143f5f2d7ead692806d727d9995e926
Anne Kariny Silva Freitas <kariny.freitas@acad.ifce.edu.br>
2018-03-17 15:26:25 -0300

    Add attributes to the ShowViewModel for details

--- TVMazeApp/TVMazeApp/ViewModel/ShowViewModel.swift
+++ TVMazeApp/TVMazeApp/ViewModel/ShowViewModel.swift
@@ -12,16 +12,32 @@ class ShowViewModel {
     
     let name: String
     var image: UIImage?
-    
+    var exibitionDayAndTime: String
+    var genders: String
+    var summary: String
     private var show: Show
     
     init(show: Show) {
         self.show = show
-        self.name = ShowViewModel.getFormattedName(from: show)
+        self.name = show.name
+        self.exibitionDayAndTime = ShowViewModel.getExibitionDayAndTime(from: show)
+        self.genders = ShowViewModel.getGendersString(from: show)
+        self.summary = ShowViewModel.getFormattedSummary(from: show)
     }
     
-    private static func getFormattedName(from show: Show) -> String {
-        return show.name
+    private static func getExibitionDayAndTime(from show: Show) -> String {
+        let days = show.days.joined(separator: ", ")
+        return "The show in on \(days) at \(show.time)"
+    }
+    
+    private static func getGendersString(from show: Show) -> String {
+        let genders = show.days.joined(separator: ", ")
+        return "Genders: \(genders)"
+    }
+    
+    private static func getFormattedSummary(from show: Show) -> String {
+        let formattedSummary = show.summary.removing(strings: ["<p>", "</p>", "<b>", "</b>"], toPut: "")
+        return formattedSummary
     }
     
     func getImage(_ completion: @escaping (UIImage?)->Void) {
