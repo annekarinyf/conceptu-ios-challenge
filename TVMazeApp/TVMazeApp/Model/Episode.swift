0224c84946dde39836a7a5a7a75de0372b6c9c99
Anne Kariny Silva Freitas <kariny.freitas@acad.ifce.edu.br>
2018-03-17 16:05:06 -0300

    Create model Episode

--- TVMazeApp/TVMazeApp/Model/Episode.swift
+++ TVMazeApp/TVMazeApp/Model/Episode.swift
@@ -9,5 +9,25 @@
 import UIKit
 
 class Episode {
-
+    var name: String
+    var number: Int
+    var season: Int
+    var image: UIImage?
+    var imageUrl: String?
+    var summary: String
+    
+    init?(json: JSON) {
+        guard let name = json["name"] as? String,
+            let number = json["number"] as? Int,
+            let season = json["season"] as? Int,
+            let imageJson = json["image"] as? JSON,
+            let imageUrl = imageJson["medium"] as? String,
+            let summary = json["summary"] as? String else { return nil }
+        
+        self.name = name
+        self.number = number
+        self.season = season
+        self.imageUrl = imageUrl
+        self.summary = summary
+    }
 }
