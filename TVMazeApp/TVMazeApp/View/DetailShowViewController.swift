abdc76f967fc829de096120e5ae4719ee77c6e26
Anne Kariny Silva Freitas <kariny.freitas@acad.ifce.edu.br>
2018-03-17 15:25:49 -0300

    Show on screen all details from the clicked show

--- TVMazeApp/TVMazeApp/View/DetailShowViewController.swift
+++ TVMazeApp/TVMazeApp/View/DetailShowViewController.swift
@@ -11,7 +11,13 @@ import UIKit
 class DetailShowViewController: UIViewController {
     
     var show: Show?
-
+    var seasons = [Episode]()
+    var showViewModel: ShowViewModel?
+    
+    @IBOutlet var showPoster: UIImageView!
+    @IBOutlet var detailsTableView: UITableView!
+    @IBOutlet var favoriteShowButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,7 +25,79 @@ class DetailShowViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         if let show = show {
-            print(show.name)
+            showViewModel = ShowViewModel(show: show)
+            checkImage()
         }
+    }
+    
+    private func checkImage(){
+        if let show = show, let image = show.image {
+            showPoster.image = image
+        } else {
+            showViewModel?.getImage({ (image) in
+                self.showPoster.image = image
+            })
+        }
+    }
+    
+    @IBAction func dismissDetailView(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension DetailShowViewController: UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 1 {
+            return "Seasons"
+        } else {
+            return nil
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+}
+
+extension DetailShowViewController: UITableViewDataSource {
+
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 4
+        } else {
+            return seasons.count
+        }
+    }
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "detailCell", for: indexPath)
+        guard let showViewModel = showViewModel else { return cell }
+        cell.textLabel?.numberOfLines = 0
+        
+        if tableView == detailsTableView {
+    
+            switch indexPath.row {
+            case 0:
+                cell.textLabel?.text = showViewModel.name
+            case 1:
+                cell.textLabel?.text = showViewModel.exibitionDayAndTime
+            case 2:
+                cell.textLabel?.text = showViewModel.genders
+            default:
+                cell.textLabel?.text = showViewModel.summary
+            }
+        } else {
+           
+        }
+         return cell
     }
 }
