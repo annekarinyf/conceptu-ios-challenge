484bb9f08abdacdbd40671d5b558c4d4b55c36b3
Anne Kariny Silva Freitas <kariny.freitas@acad.ifce.edu.br>
2018-03-17 21:51:50 -0300

    Create a CoreDataStack to manage all the CoreData operations

---
+++ TVMazeApp/TVMazeApp/Common/Extensions/CoreDataStackExtension.swift
@@ -0,0 +1,86 @@
+//
+//  CoreDataStackExtension.swift
+//  TVMazeApp
+//
+//  Created by Anne Kariny Silva Freitas on 17/03/2018.
+//  Copyright © 2018 Anne Kariny Silva Freitas. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension CoreDataStack {
+    
+    private static func show(fromShowDB showDB: ShowDB) -> Show {
+        let derivedShow = Show(id:Int(showDB.id),
+                               name: showDB.name ?? "",
+                               imageUrl: showDB.imageUrl ?? "",
+                               time: showDB.time ?? "",
+                               days: showDB.days as? [String] ?? [],
+                               genres: showDB.genres as? [String] ?? [],
+                               summary: showDB.summary ?? "")
+        
+        return derivedShow
+    }
+    
+    private static func showDB(fromShow show: Show) -> ShowDB {
+        let showForDB = ShowDB(context: context)
+        
+        showForDB.id = Int64(show.id)
+        showForDB.name = show.name
+        showForDB.imageUrl = show.imageUrl
+        showForDB.time = show.time
+        showForDB.days = show.days as NSObject?
+        showForDB.genres = show.genres as NSObject?
+        
+        return showForDB
+    }
+    
+    // MARK: - Read
+    static func read() -> [Show]? {
+        
+        let fetchRequest: NSFetchRequest<ShowDB> = ShowDB.fetchRequest()
+        fetchRequest.relationshipKeyPathsForPrefetching = ["manySeasons"]
+        
+        do {
+            let result = try context.fetch(fetchRequest)
+            return result.map { show(fromShowDB: $0) }
+        } catch {
+            return nil
+        }
+    }
+    
+    // MARK: - Save
+    static func save(_ show: Show) {
+        var existsOnDB = false
+        if let savedShows = CoreDataStack.read() {
+            for savedShows in savedShows {
+                if savedShows.id == show.id {
+                    existsOnDB = true
+                }
+            }
+        }
+        if !existsOnDB {
+            _ = showDB(fromShow: show)
+            saveContext()
+        }
+    }
+    
+    // MARK: - Delete
+    
+    static func delete(withId id: Int) {
+        do {
+            let fetchRequest: NSFetchRequest<ShowDB> = ShowDB.fetchRequest()
+            fetchRequest.predicate = NSPredicate(format: "id == \(id)")
+            let result = try context.fetch(fetchRequest)
+            
+            for showDB in result {
+                context.delete(showDB)
+            }
+        } catch {
+            print("Error on fetch")
+            delete(withId: id)
+        }
+    }
+
+}
---
+++ TVMazeApp/TVMazeApp/Database/CoreDataStack.swift
@@ -0,0 +1,63 @@
+//
+//  CoreDataStack.swift
+//  TVMazeApp
+//
+//  Created by Anne Kariny Silva Freitas on 17/03/2018.
+//  Copyright © 2018 Anne Kariny Silva Freitas. All rights reserved.
+//
+
+import Foundation
+
+import CoreData
+
+enum LogicOperator: String {
+    case and = "AND"
+    case or = "OR"
+}
+
+class CoreDataStack {
+    
+    static var context: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    static var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "TVMazeApp")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - Saving
+    static func saveContext () {
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+    
+    // MARK: - Deleting
+    static func delete (object: NSManagedObject) {
+        context.delete(object)
+    }
+    
+    static func delete(forEntityName entityName: String) {
+        let deleteFetch = NSFetchRequest<NSFetchRequestResult>(entityName: entityName)
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: deleteFetch)
+        
+        do {
+            try context.execute(deleteRequest)
+            try context.save()
+        } catch {
+            print ("There was an error")
+        }
+    }
+}
+
